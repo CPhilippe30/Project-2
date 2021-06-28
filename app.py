b50e49b79625e631379b3973fe20bb68bd9e2588
Carline Philippe <79476956+CPhilippe30@users.noreply.github.com>
2021-06-28 16:15:28 -0400

    Uploading

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
-from flask import Flask
+from logging import debug
+from flask import Flask, jsonify, render_template
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
 
+
+# database setup
+db = SQLAlchemy(app)
+
+class Arrest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return "NFL Arrests"
 
+@app.route('/api/arrests/postgres')
+def arrest_postgres():
+    arrests = db.session.query(Arrest)
+    data = []
+
+    for arrest in arrests:
+        data.append({
+            "id": arrest.id,
+            "content": arrest.content
+        })
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
