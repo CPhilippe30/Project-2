72dc39f88c2141bfa20540a23df5947c46504950
Carline Philippe <79476956+CPhilippe30@users.noreply.github.com>
2021-06-28 17:25:38 -0400

    add postgress intergration

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from os import environ
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project2-group-5.db'
 
 
 # database setup
