745386a3cda2844592526a03636bba1c5c9cee66
Carline Philippe <79476956+CPhilippe30@users.noreply.github.com>
2021-07-10 11:32:03 -0400

    update app.js

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with open('static/data/nfl-dui2.json') as f:
 
 @app.route("/")
 def index():
-    return render_template("linechart.html")
+    return render_template("index.html")
 
 @app.route("/main", methods=['GET'])
 def main():
--- static/app.js
+++ static/app.js
@@ -37,18 +37,14 @@ function initdropdown(){
         var display = d3.select("#selDataset");
         names.forEach((data) => {
             display.append("option").text(data).property("value", data);
-            
-});
-
-})
-
+        });
+    })
 }
 
 //end function initdropdown
 function optionChanged(id){
     metadata(id);
-    createChart(id);
-    createBubbles(id);
+    createBarChart(id);
 }
 
 function metadata(sampleid){
@@ -67,31 +63,167 @@ function metadata(sampleid){
     })
 }
 
+function createBarChart(yrid){
+    // Use d3.json() to fetch data from JSON file
+      d3.json("../static/data/nfl-dui2.json").then((duiData) => {
+          function filterDuiData(dui) {
+          return dui.Year == yrid;
+          }
+          // Use filter() to pass the function as its argument
+          var filteredDui = duiData.filter(filterDuiData);
+          // Filtering Dui.
+          console.log(filteredDui);
+          var teams = filteredDui.map(category =>  category.TEAM);
+          var category = filteredDui.map(category => category.Severity);
+          // Filtered metascores.
+          console.log(category);
+          // Create your trace.
+          var trace = {
+          x: teams,
+          y: category,
+          type: "bar"
+          };
+          // Create the data array for our plot
+          var data = [trace];
+          // Define the plot layout
+          var layout = {
+          title: "Teams with highest DUI Arrested.",
+          xaxis: { teams: "Team" },
+          yaxis: { category: "NFL Team (Dui) Arrests"}
+          };
+          // Plot the chart to a div tag with id "bar-plot"
+          Plotly.newPlot("bar", data, layout);
+      });
+  }
+ 
+  function metadata(yrid){
+      d3.json("../static/data/nfl-dui2.json").then(function(data){
+          console.log("in metadata")
+          //filter the year
+          function filterDuiData(d) {
+              return d.Year == yrid;
+          }  
+          var yr = data.filter(filterDuiData)
+          var metadata = yr;
+         // console.log(metadata)
+          var result = metadata;
+          var display = d3.select("#sample-metadata");
+          display.html("");
+          for (i=0; i<result.length; i++){
+              Object.entries(result[i]).forEach(([key, value]) => {
+                  display.append('div');
+                  display.append("h5").html(`<b>${key}</b>: ${value}`);
+              })//end forEach
+              display.append('hr');
+          }
+      })//end d3
+  }//end function
+
+  function metadata(yrid){
+    d3.json("../static/data/nfl-dui2.json").then(function(data){
+        console.log("in metadata")
+        //filter the year
+        function filterDuiData(d) {
+            return d.Year == yrid;
+        }  
+        var yr = data.filter(filterDuiData)
+        var metadata = yr;
+       // console.log(metadata)
+        var result = metadata;
+        var display = d3.select("#sample-metadata");
+        display.html("");
+        for (i=0; i<result.length; i++){
+            Object.entries(result[i]).forEach(([key, value]) => {
+                display.append('div');
+                display.append("h5").html(`<b>${key}</b>: ${value}`);
+            })//end forEach
+            display.append('hr');
+        }
+    })//end d3
+}//end function
 
 
-function linegraph(year){
-    var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var dui = [];
-    for (var i = 0; i <= 12; i++) {
-      dui[i] = 0
-    }
-    console.log("results.length")
-    console.log(results.length)
-    for (var i = 0; i < results.length; i++) {
-      m = results[i].Month
-      dui[m] += 1
-    }
-    console.log(month, "= ", dui)
-    var line_trace = {
-      x: month,
-      y: dui,
-      type: 'scatter'
-    };
-    var line_layout = {
-      title: `NFL Monthly DUI in ${selected_yr}`,
-    };
-    var data = [line_trace];
-    Plotly.newPlot('line', data, line_layout);
+
+function initdropdown(){
+    d3.json("../static/data/nfl-dui2.json").then(function(data){
+        //load display values
+        var display = d3.select("#selDataset");
+        for( i=2000; i<2018; i++) {
+            display.append("option").text(i).property("value", i);
+        }
+        ///get selected text
+        var e = document.getElementById("selDataset");
+        var strSelected = e.value;
+        //  filter selected year from data values
+        function filterDuiData(d) {
+            return d.Year == strSelected;
+        }  
+        var yr = data.filter(filterDuiData)
+        var year = yr[0].Year;
+        metadata(year);
+        createBarChart(year);
+      //  createBubbles(id);
+        gauge(year);
+        lineGraph(year)
+    })//end d3.json 
+}//end function initdropdown
 
 
-}
+function gauge(yrid){
+    d3.json("../static/data/nfl-dui2.json").then(function(data){
+       // console.log("in metadata")
+       // console.log(data)
+        function filterDuiData(d) {
+            return d.Year == yrid;
+        }  
+        var yrdetails = data.filter(filterDuiData) 
+        console.log(yrdetails)
+        var metadata = yrdetails[0];
+       //if severity category is 0 add 1
+        var duicat = metadata.Severity;
+        if(duicat == 0){
+            duicat +=1
+            metadata.Severity+=1
+        }
+        var title = metadata.TEAM
+        console.log('duicat')
+        console.log(duicat)
+        var data = [{
+          type: "indicator",
+          mode: "gauge",
+          value: duicat,
+          gauge: {
+            axis: { range: [null, 5], tickwidth: 1, tickcolor: "darkblue" },
+            bar: { color: "darkblue" },
+            bgcolor: "white",
+            borderwidth: 2,
+            bordercolor: "gray",
+            steps: [
+                { range: [0, 1], color: "rgba(0,128,128,.05)" },
+                { range: [1, 2], color: "rgba(0,128,128,.1)"  },
+                { range: [2, 3], color: "rgba(0,128,128,.15)" },
+                { range: [3, 4], color: "rgba(0,128,128,.20)" },
+                { range: [4, 5], color: "rgba(0,128,128,.25)" }
+            ],//end steps
+            title: 'Auto-Resize',
+            font: {
+            size: 16
+            },
+            threshold: {
+              line: { color: "red", width: 4 },
+              thickness: 0.75,
+              value: duicat
+            }//end threshold
+          }//end gauge
+        }]; //data
+      var layout = {
+        width: 500,
+        height: 400,
+       // margin: { t: 0, r: 0, l: 0, b: 0 },
+        font: { color: "darkblue", family: "Arial" },
+        title: { text: title, font: { size: 24 }}
+        //Sub title should be Team name
+      };//end layout
+      Plotly.newPlot('gauge', data, layout);
+    })//end d3
+}//end function
